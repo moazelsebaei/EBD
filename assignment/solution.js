19ae86c7a4752edd066a7c311400d5657023ea1b
{Your_Name} <{your.email@student.giu-uni.de}moaz.elsebaei@student.giu-uni.de>
2026-09-24 21:11:28 +0300

    Add solution.js with hero details

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+const heroName = "moaz";
+const studentId = "16005191";
+const power = Number(studentId) * 8;
+const warcry = "By the Delta marshes, the Stone Golem falls!";
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
